019d5e769e137fe91eb2508754207a8e336df48d
pv <pv@pop-os.localdomain>
2020-05-07 09:25:28 -0500

    add song detail

--- src/Components/SongList.js
+++ src/Components/SongList.js
@@ -1,17 +1,18 @@
 import React,{Component}    from 'react';
 // connect componenent used to create connection with Provider  
 import {connect} from 'react-redux';
-
+import {selectSong} from '../actions';
 
 class SongList extends Component {
    
     renderList(){
          // this.props === {songs: state.songs}
-        return this.props.songs.map(song =>{
+        return this.props.songs.map(song=>{
             return (
                 <div className ="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">Select</button>
+                        <button className="ui button primary" 
+                        onClick = {()=>this.props.selectSong(song)}>Select</button>
                     </div>
 
                     <div className="content">{song.title}</div>
@@ -22,7 +23,7 @@ class SongList extends Component {
 
     render(){
        // props will include dispatch function from store  
-    //   console.log(this.props)
+     
         return (
             <div className=" ui divided list">
                {this.renderList()}
@@ -36,11 +37,13 @@ class SongList extends Component {
 // function can be named anything
 // it will pass state to props so that child component can render the state
 const mapStateToProps = (state )=>{
-    // console.log(state); 
+    console.log(state); 
     //this will return objecs  {selectedSong, songs} from reducers
     // this.props === state
     return {songs: state.songs};
 }   
 
 // connect() return a function , the second () invokes the fuction that got return
-export default connect(mapStateToProps)(SongList);
+//pass state, action from high order component:connect -> to the component need
+//connect component call store.dispatch(selectSong) to the reducers
+export default connect(mapStateToProps,{selectSong})(SongList);
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 // Actions Creators
 
 // named export to  export functions from single file
-export const selectSong = ()=>{
+export const selectSong = (song)=>{
     // return object action: {type, payload}
     return {
-        type: 'SELECTED_SONG',
+        type: "SELECTED_SONG",
         payload: song,
     }
 };
