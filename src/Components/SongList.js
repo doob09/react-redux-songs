4eefd01f1a33a97045c11c8475773dee18880a80
pv <pv@pop-os.localdomain>
2020-05-06 21:49:30 -0500

    fix SongList

--- src/Components/SongList.js
+++ src/Components/SongList.js
@@ -6,13 +6,12 @@ import {connect} from 'react-redux';
 class SongList extends Component {
    
     renderList(){
-     
+         // this.props === {songs: state.songs}
         return this.props.songs.map(song =>{
             return (
                 <div className ="item" key={song.title}>
                     <div className="right floated content">
                         <button className="ui button primary">Select</button>
-                        
                     </div>
 
                     <div className="content">{song.title}</div>
@@ -37,9 +36,9 @@ class SongList extends Component {
 // function can be named anything
 // it will pass state to props so that child component can render the state
 const mapStateToProps = (state )=>{
-    console.log(state); 
+    // console.log(state); 
     //this will return objecs  {selectedSong, songs} from reducers
-    // this.props = state
+    // this.props === state
     return {songs: state.songs};
 }   
 
